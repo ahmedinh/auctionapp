aa3d38a6b457fd66aa606bf863765e70706c5cff
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-14 12:06:22 +0100

    Renamed BackendApplication to AuctionApplication

---
+++ backend/src/main/java/ba/atlant/auctionapp/backend/AuctionApplication.java
@@ -0,0 +1,15 @@
+package ba.atlant.auctionapp.backend;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionApplication.class, args);
+    }
+
+}
