a768b60e200f5c472bc24dd844966e839e431d3d
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-16 14:12:28 +0100

    Style modifications to white navbar

--- frontend/src/components/header/NavbarWhite.jsx
+++ frontend/src/components/header/NavbarWhite.jsx
@@ -6,16 +6,20 @@ import Magnifier from "../icons/Magnifier";
 const NavbarWhite = () => {
     return (
         <div className="navbar-white">
-            <img src={Logo} alt="Logo" className="logo"/>
+            <div className="left-container">
+                <img src={Logo} alt="Logo" className="logo"/>
+            </div>
             <div className="search-container">
                 <input type="text" placeholder="Try enter: Shoes" className="search-bar"/>
                 <button className="search-icon">
                     <Magnifier/>
                 </button>
             </div>
-            <button className="nav-btn">HOME</button>
-            <button className="nav-btn">SHOP</button>
-            <button className="nav-btn">MY ACCOUNT</button>
+            <div className="buttons">
+                <button className="nav-btn">HOME</button>
+                <button className="nav-btn">SHOP</button>
+                <button className="nav-btn">MY ACCOUNT</button>
+            </div>
         </div>
     );
 };
