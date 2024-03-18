85aefdacde43a4b72777483890abdd61c1c2906a
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-18 15:42:20 +0100

    Added color to chosen button

--- frontend/src/components/header/NavbarWhite.jsx
+++ frontend/src/components/header/NavbarWhite.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./NavbarWhite.scss";
 import Logo from "../icons/logo.png";
 import Magnifier from "../icons/Magnifier";
 
 const NavbarWhite = () => {
+    const location = useLocation(); // Get the current location object
+    const currentPath = location.pathname; // Extract the path from location
+
+    // Determine if the current path matches one of the specified routes
+    const isSpecialRoute = ['/about-us', '/privacy-and-policy', '/terms-and-conditions'].includes(currentPath);
+    
     return (
         <div className="navbar-white">
             <div className="left-container">
@@ -16,7 +23,7 @@ const NavbarWhite = () => {
                 </button>
             </div>
             <div className="buttons">
-                <button className="nav-btn">HOME</button>
+                <button className={`nav-btn ${isSpecialRoute ? 'special-route' : ''}`}>HOME</button>
                 <button className="nav-btn">SHOP</button>
                 <button className="nav-btn">MY ACCOUNT</button>
             </div>
