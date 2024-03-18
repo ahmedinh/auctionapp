322e3cebbe559191774b8f19b87d265893c700e5
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-18 23:53:03 +0100

    Added Getter and Setter annotation

--- backend/src/main/java/ba/atlant/auctionapp/model/Category.java
+++ backend/src/main/java/ba/atlant/auctionapp/model/Category.java
@@ -1,11 +1,15 @@
 package ba.atlant.auctionapp.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+@Getter
+@Setter
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
