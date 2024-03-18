15ccf34ed7a24b77a555a2d5505e5eaaae4eb4e7
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-18 23:50:11 +0100

    Corrected Bid class according to inputs

--- backend/src/main/java/ba/atlant/auctionapp/model/Bid.java
+++ backend/src/main/java/ba/atlant/auctionapp/model/Bid.java
@@ -2,22 +2,31 @@ package ba.atlant.auctionapp.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bid {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Min(value = 0, message = "Amount cannot be negative.")
-    private Double amount;
+    @PositiveOrZero(message = "Bid amount must be greater or equal to 0.")
+    @NotNull(message = "Bid amount cannot be null.")
+    private BigDecimal amount;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
 }
