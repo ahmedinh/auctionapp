200b10d7030bc524e11433083064adfd78ec1ed5
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-18 23:57:42 +0100

    Updated pattern regex

--- backend/src/main/java/ba/atlant/auctionapp/model/Category.java
+++ backend/src/main/java/ba/atlant/auctionapp/model/Category.java
@@ -19,7 +19,7 @@ public class Category {
     @NotNull(message = "Category name must not be null")
     @NotEmpty(message = "Category name must not be empty")
     @Size(min = 2, max = 30, message = "Category name must be between 2 and 255 characters long")
-    @Pattern(regexp = "^[A-Z][a-zA-Z0-9]*$", message = "Category name must start with a capital letter and only contain alphanumeric characters and spaces")
+    @Pattern(regexp = "^[A-Z][a-zA-Z0-9 ',-]*$", message = "Category name must start with a capital letter and only contain alphanumeric characters, spaces, apostrophes, or hyphens")
     private String name;
 
     @OneToMany(mappedBy = "category")
