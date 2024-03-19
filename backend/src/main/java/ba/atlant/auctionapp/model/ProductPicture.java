663620fc1eb119ee8b29add941fd56dc38cdee2b
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-19 01:15:06 +0100

    Updated according to comments

--- backend/src/main/java/ba/atlant/auctionapp/model/ProductPicture.java
+++ backend/src/main/java/ba/atlant/auctionapp/model/ProductPicture.java
@@ -1,17 +1,22 @@
 package ba.atlant.auctionapp.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ProductPicture {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String url;
 
     @ManyToOne
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
-
 }
