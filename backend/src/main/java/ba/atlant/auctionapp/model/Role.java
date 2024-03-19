f2a437435b0ad64fa629991af938d2e4cba0293f
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-19 01:15:45 +0100

    Added constructors, getters and setters

--- backend/src/main/java/ba/atlant/auctionapp/model/Role.java
+++ backend/src/main/java/ba/atlant/auctionapp/model/Role.java
@@ -1,11 +1,15 @@
 package ba.atlant.auctionapp.model;
 
 import jakarta.persistence.*;
-
+import lombok.*;
 import java.util.List;
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
