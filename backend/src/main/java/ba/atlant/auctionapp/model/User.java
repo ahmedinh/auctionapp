7200ddd130d2536342e591e12cfda246acd38704
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-19 01:16:56 +0100

    Added size constraints to name

--- backend/src/main/java/ba/atlant/auctionapp/model/User.java
+++ backend/src/main/java/ba/atlant/auctionapp/model/User.java
@@ -22,12 +22,13 @@ public class User {
 
     @NotBlank(message = "First name cannot be blank.")
     @NotNull(message = "First name cannot be null.")
+    @Size(min = 2, max = 30, message = "First name must be between 2 and 30 characters long.")
     @Column(name = "first_name")
     private String firstName;
 
-
     @NotBlank(message = "Last name cannot be blank.")
     @NotNull(message = "Last name cannot be null.")
+    @Size(min = 2, max = 30, message = "Last name must be between 2 and 30 characters long.")
     @Column(name = "last_name")
     private String lastName;
 
