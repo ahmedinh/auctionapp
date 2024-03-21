ae688f0b06dff4ecefa7ee70b85263f977845c3a
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-21 16:24:21 +0100

    Added file with common messages

---
+++ frontend/src/components/messages/Text.jsx
@@ -0,0 +1,9 @@
+export const AboutUsPage = {
+    dummy_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna. In convallis diam volutpat libero tincidunt semper. Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie. In id elementum turpis. Curabitur quis tincidunt mauris."
+};
+
+export const common = {
+    about_us: "About Us",
+    terms_and_conditions: "Terms And Conditions",
+    privacy_and_policy: "Privacy And Policy"
+};
