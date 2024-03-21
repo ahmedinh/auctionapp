36385f725cc1a64fa25b600db2ca684ad5d0d1fa
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-21 16:23:20 +0100

    Added breadcrumbs

---
+++ frontend/src/components/utilities/Breadcrumbs.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import { GoArrowRight } from "react-icons/go";
+import "./Breadcrumbs.scss";
+
+export default function Breadcrumbs() {
+    const location = useLocation();
+
+    const formatCrumb = (crumb) => {
+        return crumb.replace(/-/g, ' ').replace(/\b\w/g, letter => letter.toUpperCase());
+    };
+
+    let currentLink = '';
+
+    const crumbs = location.pathname.split('/')
+        .filter(crumb => crumb !== '')
+        .map((crumb, index, array) => {
+            currentLink += `/${crumb}`;
+            const isLast = index === array.length - 1;
+
+            return (
+                <React.Fragment key={crumb}>
+                    <Link to={currentLink} className={`crumb ${isLast ? 'current' : ''}`}>
+                        {formatCrumb(crumb)}
+                    </Link>
+                    {index < array.length - 1 && <GoArrowRight className="arrow" />}
+                </React.Fragment>
+            );
+        });
+
+    return (
+        <div className="breadcrumbs" style={{ display: 'flex', alignItems: 'center' }}>
+            {crumbs}
+        </div>
+    );
+};
