31cee312e76e7a8fbfb971ad4127519ad3e2df9f
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-22 16:16:43 +0100

    Added layout with breadcrumbs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,14 @@ import Terms from './components/pages/Terms';
 import Privacy from './components/pages/Privacy';
 import Home from './components/pages/Home';
 import Layout from './components/utilities/Layout';
+import Page from './components/pages/Page';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="home">
+      <Route path="home" element={<Home />} />
+      <Route path="home" element={<Page />}>
         <Route path="about-us" element={<AboutUs />} />
         <Route path="terms-and-conditions" element={<Terms />} />
         <Route path="privacy-and-policy" element={<Privacy />} />
@@ -20,6 +22,6 @@ const router = createBrowserRouter(
 );
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 };
 export default App;
