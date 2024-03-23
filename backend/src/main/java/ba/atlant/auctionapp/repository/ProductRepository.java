89bafcff660b2068c95516cf60944fc00fdd7984
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-24 00:34:32 +0100

    Create ProductRepository.java

---
+++ backend/src/main/java/ba/atlant/auctionapp/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package ba.atlant.auctionapp.repository;
+
+import ba.atlant.auctionapp.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, PagingAndSortingRepository<Product, Long> {
+}
