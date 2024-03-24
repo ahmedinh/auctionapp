7fe2cbc46a4ecb4ec8d6c089b690afc56ba6361d
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-24 01:01:40 +0100

    Create UserRepository.java

---
+++ backend/src/main/java/ba/atlant/auctionapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ba.atlant.auctionapp.repository;
+
+import ba.atlant.auctionapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long> {
+}
