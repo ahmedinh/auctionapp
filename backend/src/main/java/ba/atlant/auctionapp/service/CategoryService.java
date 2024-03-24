b79e967488b7a7fec59e4b204bd278ed130fba65
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-24 13:22:40 +0100

    Create CategoryService.java

---
+++ backend/src/main/java/ba/atlant/auctionapp/service/CategoryService.java
@@ -0,0 +1,22 @@
+package ba.atlant.auctionapp.service;
+
+import ba.atlant.auctionapp.model.Category;
+import ba.atlant.auctionapp.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public ResponseEntity getAllCategories() {
+        List<Category> categoryList = categoryRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(categoryList);
+    }
+}
