366773c0871e3c24613ea17741a75a8dae82e824
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-25 13:33:19 +0100

    New file for routes

---
+++ frontend/src/components/utilities/Router.jsx
@@ -0,0 +1,28 @@
+import AboutUs from '../pages/AboutUs';
+import Terms from '../pages/Terms';
+import Privacy from '../pages/Privacy';
+import Home from '../pages/HomePage/Home';
+import Layout from './Layout';
+import Page from '../pages/Page';
+import NewArrivals from '../pages/HomePage/NewArrivals';
+import LastChance from '../pages/HomePage/LastChance';
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+
+const Router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index path="home" element={<Home />}/>
+        <Route path="home" element={<Home/>}>
+          <Route path="new-arrivals" element={<NewArrivals/>}/>
+          <Route path="last-chance" element={<LastChance/>}/>
+        </Route>
+        <Route path="home" element={<Page />}>
+          <Route path="about-us" element={<AboutUs />} />
+          <Route path="terms-and-conditions" element={<Terms />} />
+          <Route path="privacy-and-policy" element={<Privacy />} />
+        </Route>
+      </Route>
+    )
+  );
+
+  export default Router;
