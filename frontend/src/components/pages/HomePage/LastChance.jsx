d4a1cec937db9f49e53f81e7a6770c77cdfea747
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-25 13:33:43 +0100

    Initial files for LastChance and NewArrivals

---
+++ frontend/src/components/pages/HomePage/LastChance.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./LastChance.scss";
+
+const LastChance = () => {
+    return (
+        <div className="class">
+            OVO JE LAST CHANCE STRANICA.
+        </div>
+    );
+};
+export default LastChance;
---
+++ frontend/src/components/pages/HomePage/NewArrivals.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NewArrivals.scss";
+
+const NewArrivals = () => {
+    return (
+        <div className="class">
+            OVO JE NEW ARRIVALS STRANICA.
+        </div>
+    );
+};
+export default NewArrivals;
