eb6ff7fe5a6e303ccc852a456b1ae39d31499b31
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-29 16:12:41 +0100

    Simple routes for product pictures

---
+++ backend/src/main/java/ba/atlant/auctionapp/dto/ProductPictureDTO.java
@@ -0,0 +1,12 @@
+package ba.atlant.auctionapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductPictureDTO {
+    private String name;
+    private String url;
+    private Long productId;
+}
