ccbb8f56a810560bf27a0230d5c5911a8095515d
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-29 16:13:27 +0100

    Dto for returning product
    This object is needed to return additional fields without multiple API requests. Fields are "largestBid" and "numberOfBids".

---
+++ backend/src/main/java/ba/atlant/auctionapp/dto/ProductDTO.java
@@ -0,0 +1,48 @@
+package ba.atlant.auctionapp.dto;
+
+import ba.atlant.auctionapp.enumeration.Color;
+import ba.atlant.auctionapp.enumeration.Size;
+import ba.atlant.auctionapp.model.Category;
+import ba.atlant.auctionapp.model.Product;
+import ba.atlant.auctionapp.model.ProductPicture;
+import ba.atlant.auctionapp.model.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal startPrice;
+    private LocalDateTime createdAt;
+    private LocalDateTime auctionStart;
+    private LocalDateTime auctionEnd;
+    private Size size;
+    private Color color;
+    private List<ProductPicture> productPictureList; // Assume this is another DTO for pictures
+    private Category category; // Assume this is a DTO for the category
+    private User user; // Assume this is a DTO for the user
+    private BigDecimal largestBid; // New field
+    private Integer numberOfBids;
+    public ProductDTO() {}
+    public ProductDTO(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.startPrice = product.getStartPrice();
+        this.createdAt = product.getCreatedAt();
+        this.auctionStart = product.getAuctionStart();
+        this.auctionEnd = product.getAuctionEnd();
+        this.size = product.getSize();
+        this.color = product.getColor();
+        this.productPictureList = product.getProductPictureList();
+        this.category = product.getCategory();
+        this.user = product.getUser();
+    }
+}
