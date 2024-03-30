683a8e5ceee8a8f0072a8ed7d3e2c4ecdf172ead
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-30 15:53:00 +0100

    Update Router.jsx

--- frontend/src/components/utilities/Router.jsx
+++ frontend/src/components/utilities/Router.jsx
@@ -5,26 +5,33 @@ import Home from '../pages/HomePage/Home';
 import Layout from './Layout';
 import NewArrivals from '../pages/HomePage/Products/NewArrivals';
 import LastChance from '../pages/HomePage/Products/LastChance';
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import Product from '../pages/Product';
+import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom";
+import ProductDetails from '../pages/ProductOverview/Details';
+import SellerInformation from '../pages/ProductOverview/SellerInformation';
+import CustomerReviews from '../pages/ProductOverview/CustomerReviews';
+import Product from '../pages/ProductOverview/Product';
 
 const Router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index path="home" element={<Home />} />
-      <Route path="home" element={<Home />}>
-        <Route path="new-arrivals" element={<NewArrivals />} />
-        <Route path="last-chance" element={<LastChance />} />
-      </Route>
-      <Route path="home">
-        <Route path="about-us" element={<AboutUs />} />
-        <Route path="terms-and-conditions" element={<Terms />} />
-        <Route path="privacy-and-policy" element={<Privacy />} />
-      </Route>
-      <Route path="shop">
-        <Route path="product/:id" element={<Product />} />
-      </Route>
-    </Route>
-  )
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate replace to="/home/new-arrivals" />} />
+            <Route path="home" element={<Home />}>
+                <Route path="new-arrivals" element={<NewArrivals />} />
+                <Route path="last-chance" element={<LastChance />} />
+            </Route>
+            <Route path="home">
+                <Route path="about-us" element={<AboutUs />} />
+                <Route path="terms-and-conditions" element={<Terms />} />
+                <Route path="privacy-and-policy" element={<Privacy />} />
+            </Route>
+            <Route path="shop">
+                <Route path="product/:id" element={<Product />}>
+                    <Route path="details" element={<ProductDetails />} />
+                    <Route path="seller-information" element={<SellerInformation />} />
+                    <Route path="reviews" element={<CustomerReviews />} />
+                </Route>
+            </Route>
+        </Route>
+    )
 );
 export default Router;
