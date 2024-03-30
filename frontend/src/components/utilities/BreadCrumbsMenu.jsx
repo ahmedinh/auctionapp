c36e346ef4a7b367dee60acf6c0722aea470224c
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-03-30 15:52:45 +0100

    Added font weight for product in breadcrumbs

--- frontend/src/components/utilities/BreadCrumbsMenu.jsx
+++ frontend/src/components/utilities/BreadCrumbsMenu.jsx
@@ -4,7 +4,7 @@ import { common } from "../../messages/Text";
 import "./BreadCrumbsMenu.scss";
 import { Outlet } from "react-router";
 
-const BreadCrumbsMenu = ({title, rightLink}) => {
+const BreadCrumbsMenu = ({title, rightLink, fontWeight = 400}) => {
     const formatTitle = (path) => {
         const paths = path.split('/').filter(Boolean);
         const lastPart = paths[paths.length - 1];
@@ -17,7 +17,7 @@ const BreadCrumbsMenu = ({title, rightLink}) => {
         <>
             <div className="menu-a">
                 <div className="left-part-a">
-                    <p className="left-part-text">
+                    <p className="left-part-text" style={{ fontWeight }}>
                         {currentPageTitle || common.about_us}
                     </p>
                 </div>
