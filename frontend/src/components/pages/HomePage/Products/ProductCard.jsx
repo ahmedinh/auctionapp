92f5ae6f4be213a5ce6cd161a96c8f53dad4f7bc
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-04-01 11:41:40 +0200

    Extracted width because of overriding

--- frontend/src/components/pages/HomePage/Products/ProductCard.jsx
+++ frontend/src/components/pages/HomePage/Products/ProductCard.jsx
@@ -4,9 +4,9 @@ import "./ProductCard.scss";
 
 const ProductCard = ({ product }) => {
     return (
-        <div className="product" key={product.id}>
+        <div className="product" key={product.id} style={{ width: "267.45px" }}>
             <NavLink to={`/shop/product/${product.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                <img src={product.productPictureList[0].url} alt={product.name} className='picture'/>
+                <img src={product.productPictureList[0].url} alt={product.name} className='picture' />
                 <h5>{product.name}</h5>
             </NavLink>
             <p>Start from <span className="price">${product.startPrice.toFixed(2)}</span></p>
