329cdb27f831c265ff221c0f998d27d8a52e4eb1
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-04-01 15:55:28 +0200

    Update ProductController.java

--- backend/src/main/java/ba/atlant/auctionapp/controller/ProductController.java
+++ backend/src/main/java/ba/atlant/auctionapp/controller/ProductController.java
@@ -1,5 +1,6 @@
 package ba.atlant.auctionapp.controller;
 
+import ba.atlant.auctionapp.dto.ProductPictureDTO;
 import ba.atlant.auctionapp.model.Product;
 import ba.atlant.auctionapp.service.ProductService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -42,5 +43,17 @@ public class ProductController {
     @GetMapping()
     public ResponseEntity getProduct(@RequestParam Long id) {
         return productService.getProduct(id);
+    }
+
+    @GetMapping("/all/category")
+    public ResponseEntity getProductsForCategory(@RequestParam(defaultValue = "0") int page,
+                                                 @RequestParam(defaultValue = "9") int size,
+                                                 @RequestParam(defaultValue = "1") Long categoryId) {
+        return productService.getProductsForCategory(page, size, categoryId);
+    }
+
+    @PostMapping("/add-picture")
+    public ResponseEntity addPictureForProduct(@Valid @RequestBody ProductPictureDTO productPictureDTO) {
+        return productService.addPictureToPictureList(productPictureDTO);
     }
 }
