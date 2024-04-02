6275647af968b1e6a73ed8d03f571a3e5a076f26
Ahmedin Hasanović <ahmedinhasanovic2000@gmail.com>
2024-04-02 12:18:06 +0200

    Update ProductPictureRepository.java

--- backend/src/main/java/ba/atlant/auctionapp/repository/ProductPictureRepository.java
+++ backend/src/main/java/ba/atlant/auctionapp/repository/ProductPictureRepository.java
@@ -1,6 +1,5 @@
 package ba.atlant.auctionapp.repository;
 
-import ba.atlant.auctionapp.model.Product;
 import ba.atlant.auctionapp.model.ProductPicture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
