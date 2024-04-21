877a6881807d4334e984edbf6d6a83f05ddd1c74
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-04-21 17:47:02 +0200

    Classes for request and response

---
+++ backend/src/main/java/ba/atlant/auctionapp/response/AuthResponse.java
@@ -0,0 +1,34 @@
+package ba.atlant.auctionapp.response;
+
+
+import ba.atlant.auctionapp.model.Person;
+
+public class AuthResponse {
+    private Person person;
+    private String token;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(Person person, String token) {
+        this.person = person;
+        this.token = token;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
