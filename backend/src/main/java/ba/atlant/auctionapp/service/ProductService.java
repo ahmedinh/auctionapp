fedd6f0e0bb9cd4ce2677b1b79f2fe5ca866a698
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-04-26 15:23:23 +0200

    Update ProductService.java

--- backend/src/main/java/ba/atlant/auctionapp/service/ProductService.java
+++ backend/src/main/java/ba/atlant/auctionapp/service/ProductService.java
@@ -1,16 +1,20 @@
 package ba.atlant.auctionapp.service;
 
 import ba.atlant.auctionapp.dto.ProductDTO;
+import ba.atlant.auctionapp.exception.ServiceException;
 import ba.atlant.auctionapp.model.*;
 import ba.atlant.auctionapp.projection.ProductProjection;
 import ba.atlant.auctionapp.repository.*;
-import ba.atlant.auctionapp.exception.ServiceException;
+import com.amazonaws.HttpMethod;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.*;
 
@@ -22,13 +26,15 @@ public class ProductService {
     private final ProductPictureRepository productPictureRepository;
     private final PersonRepository personRepository;
     private final BidRepository bidRepository;
+    private final S3Service s3Service;
 
-    public ProductService(ProductRepository productRepository, SubCategoryRepository subCategoryRepository, ProductPictureRepository productPictureRepository, PersonRepository personRepository, BidRepository bidRepository) {
+    public ProductService(ProductRepository productRepository, SubCategoryRepository subCategoryRepository, ProductPictureRepository productPictureRepository, PersonRepository personRepository, BidRepository bidRepository, S3Service s3Service) {
         this.productRepository = productRepository;
         this.subCategoryRepository = subCategoryRepository;
         this.productPictureRepository = productPictureRepository;
         this.personRepository = personRepository;
         this.bidRepository = bidRepository;
+        this.s3Service = s3Service;
     }
 
     @Transactional
@@ -99,5 +105,24 @@ public class ProductService {
 
     public ResponseEntity<Page<ProductProjection>> searchProducts(int page, int size, String query) {
         return ResponseEntity.ok(productRepository.searchProducts(query, PageRequest.of(page,size)));
+    }
+
+    @Transactional
+    public ResponseEntity<List<ProductPicture>> addProductPictures(MultipartFile[] files, Long productId) throws IOException {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if (optionalProduct.isEmpty())
+            throw new IllegalArgumentException("No product found with required ID.");
+        Product product = optionalProduct.get();
+        List<ProductPicture> productPictureList = new ArrayList<>();
+        for (MultipartFile file : files) {
+            s3Service.uploadFile(file.getOriginalFilename(), file);
+            productPictureList.add(new ProductPicture(
+                    file.getOriginalFilename(),
+                    String.format("https://%s.s3.%s.amazonaws.com/%s",s3Service.getBucketName(),s3Service.getRegion(),file.getOriginalFilename()),
+                    product
+            ));
+        }
+        productPictureRepository.saveAll(productPictureList);
+        return ResponseEntity.ok(productPictureList);
     }
 }
