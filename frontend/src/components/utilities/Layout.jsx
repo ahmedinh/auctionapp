838a99e1f80e9c0547b8918d220e6aed975fb8bb
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-04-27 15:33:10 +0200

    Updated layout and router for new pages

--- frontend/src/components/utilities/Layout.jsx
+++ frontend/src/components/utilities/Layout.jsx
@@ -3,11 +3,20 @@ import NavbarBlack from '../header/NavbarBlack';
 import NavbarWhite from '../header/NavbarWhite';
 import Footer from '../footer/Footer';
 import { Outlet } from 'react-router-dom';
+import NavbarBlackLogged from '../header/NavbarBlackLogged';
+import { useState, useEffect } from 'react';
 
 const Layout = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const user = localStorage.getItem('user');
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!user && !!token);
+    }, []);
     return (
         <>
-            <NavbarBlack />
+            {isLoggedIn ? <NavbarBlackLogged /> : <NavbarBlack />}
             <NavbarWhite />
             <main><Outlet /></main>
             <Footer />
