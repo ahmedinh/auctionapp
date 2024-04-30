c8d0ba95ece8b867ef15cc1718fdc04168175292
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-04-30 12:30:38 +0200

    Update NavbarBlack.jsx

--- frontend/src/components/header/NavbarBlack.jsx
+++ frontend/src/components/header/NavbarBlack.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./NavbarBlack.scss";
 import SocialIcons from "../../assets/icons/SocialIcons";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { getToken, removeSession, validToken } from "../utilities/Common";
 
 const NavbarBlack = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (getToken() && !validToken()) {
+            removeSession();
+            alert('Your session has expired! Please login again.')
+            navigate('/home/new-arrivals')
+        }
+    }, []);
     return (
         <nav className="navbar-black">
             <div className="nav-black-content">
