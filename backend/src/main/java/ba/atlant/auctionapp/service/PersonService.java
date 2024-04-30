53aa8553975b7d95e84c38eba998480e62ac1d91
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-04-30 12:30:03 +0200

    Update PersonService.java

--- backend/src/main/java/ba/atlant/auctionapp/service/PersonService.java
+++ backend/src/main/java/ba/atlant/auctionapp/service/PersonService.java
@@ -1,14 +1,17 @@
 package ba.atlant.auctionapp.service;
 
 import ba.atlant.auctionapp.config.jwt.JwtUtils;
+import ba.atlant.auctionapp.dto.PersonDTO;
 import ba.atlant.auctionapp.enumeration.Role;
 import ba.atlant.auctionapp.exception.EmailAlreadyUsedException;
 import ba.atlant.auctionapp.model.Person;
+import ba.atlant.auctionapp.projection.PersonProjection;
 import ba.atlant.auctionapp.repository.PersonRepository;
 import ba.atlant.auctionapp.request.AuthRequest;
 import ba.atlant.auctionapp.request.LoginRequest;
 import ba.atlant.auctionapp.request.RegisterRequest;
 import ba.atlant.auctionapp.response.AuthResponse;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -17,8 +20,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class PersonService {
@@ -26,12 +36,14 @@ public class PersonService {
     final JwtUtils jwtUtils;
     private final PersonRepository personRepository;
     private final PasswordEncoder passwordEncoder;
+    private final S3Service s3Service;
 
-    public PersonService(PersonRepository personRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+    public PersonService(PersonRepository personRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils, S3Service s3Service) {
         this.personRepository = personRepository;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
+        this.s3Service = s3Service;
     }
 
     public ResponseEntity<String> addUser(Person person) {
@@ -80,5 +92,77 @@ public class PersonService {
     }
     public Person getByEmail(String email) {
         return personRepository.findByEmail(email).orElse(null);
+    }
+
+    Integer getUserId(String authHeader) {
+        String token = authHeader.substring(7);
+        return jwtUtils.getUserIdFromJwtToken(token);
+    }
+
+    public ResponseEntity<PersonProjection> getCurrentUser(String authHeader) {
+        Integer userId = getUserId(authHeader);
+        Optional<Person> optionalPerson = personRepository.findById(Long.valueOf(userId.toString()));
+        if (optionalPerson.isEmpty())
+            throw new IllegalArgumentException("No user found with provided ID.");
+        PersonProjection personProjection = personRepository.getPersonInformation(Long.valueOf(userId.toString()));
+        return ResponseEntity.ok(personProjection);
+    }
+
+    @Transactional
+    public ResponseEntity<PersonDTO> modifyCurrentUser(String authHeader, PersonDTO personDTO) {
+        try {
+            Integer userId = getUserId(authHeader);
+            Optional<Person> optionalPerson = personRepository.findById(Long.valueOf(userId));
+
+            if (optionalPerson.isEmpty())
+                throw new IllegalArgumentException("No user found with provided ID.");
+            Person person = optionalPerson.get();
+
+            person.setFirstName(personDTO.getFirstName());
+            person.setLastName(personDTO.getLastName());
+            person.setBirthDate(LocalDate.of(personDTO.getBirthYear(), personDTO.getBirthMonth(), personDTO.getBirthDay()));
+            person.setPhoneNumber(personDTO.getPhoneNumber());
+            person.setShippingAddress(personDTO.getShippingStreet());
+            person.setZipCode(personDTO.getShippingZipCode());
+            person.setShippingCity(personDTO.getShippingCity());
+            person.setState(personDTO.getShippingState());
+            person.setCountry(personDTO.getShippingCountry());
+            person.setExpirationYear(personDTO.getExpirationYear());
+            person.setExpirationMonth(personDTO.getExpirationMonth());
+            person.setCardName(personDTO.getCardName());
+            person.setCVV(personDTO.getCvc());
+            person.setCardNumber(personDTO.getCardNumber());
+
+            personRepository.save(person);
+            return ResponseEntity.ok(personDTO);
+        } catch (Exception e) {
+            System.out.println("Error modifying user: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<Person> addPictureToUser(String authHeader, MultipartFile file) throws IOException {
+        Integer userId = getUserId(authHeader);
+        Optional<Person> optionalPerson = personRepository.findById(Long.valueOf(userId));
+        if (optionalPerson.isEmpty())
+            throw new IllegalArgumentException("No user found with provided ID.");
+        Person person = optionalPerson.get();
+        s3Service.deleteObject(person.getPictureUrl());
+        s3Service.uploadFile(file.getOriginalFilename(), file);
+        person.setPictureUrl(String.format("https://%s.s3.%s.amazonaws.com/%s",s3Service.getBucketName(),s3Service.getRegion(),file.getOriginalFilename()));
+        personRepository.save(person);
+        return ResponseEntity.ok(person);
+    }
+
+    public ResponseEntity<Map<String,String>> getUserPicture(String authHeader) {
+        Integer userId = getUserId(authHeader);
+        Optional<Person> optionalPerson = personRepository.findById(Long.valueOf(userId));
+        if (optionalPerson.isEmpty())
+            throw new IllegalArgumentException("No user found with provided ID.");
+        Person person = optionalPerson.get();
+        Map<String, String> map = new HashMap<>();
+        map.put("url", person.getPictureUrl());
+        return ResponseEntity.ok(map);
     }
 }
