061d4cf0b11478dff39da37100025e6e4e32c208
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-04-30 12:29:29 +0200

    Update BidRepository.java
    query for all bids placed by logged in usre

--- backend/src/main/java/ba/atlant/auctionapp/repository/BidRepository.java
+++ backend/src/main/java/ba/atlant/auctionapp/repository/BidRepository.java
@@ -1,11 +1,30 @@
 package ba.atlant.auctionapp.repository;
 
 import ba.atlant.auctionapp.model.Bid;
+import ba.atlant.auctionapp.model.Person;
+import ba.atlant.auctionapp.projection.BidProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BidRepository extends JpaRepository<Bid, Long>, PagingAndSortingRepository<Bid, Long> {
     List<Bid> findAllByProductId(Long id);
+
+    List<Bid> findAllByPerson(Person person);
+
+    @Query("""
+            SELECT b.product.id as id,
+            b.product.name as productName,
+            (select pp.url from ProductPicture pp WHERE pp.id = (select min(pp2.id) FROM ProductPicture pp2 WHERE pp2.product.id = b.product.id)) as productPictureUrl,
+            b.product.auctionEnd as auctionEnd,
+            b.amount as userPrice,
+            (SELECT COUNT(bb.id) FROM Bid bb WHERE bb.product.id = b.product.id) as noOfBids,
+            (SELECT MAX(bb.amount) FROM Bid bb WHERE bb.product.id = b.product.id) as maxBid
+            FROM Bid b
+            WHERE b.person.id = :userId
+            """)
+    List<BidProjection> getUserBids(@Param("userId") Long userId);
 }
