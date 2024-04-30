419604c90a28a1400822c41dae43705b52db11d2
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-04-30 12:27:52 +0200

    Create PersonDTO.java
    this dto is needed for put method for changing user info

---
+++ backend/src/main/java/ba/atlant/auctionapp/dto/PersonDTO.java
@@ -0,0 +1,59 @@
+package ba.atlant.auctionapp.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonDTO {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Min(value = 1, message = "Birth day must be at least 1")
+    @Max(value = 31, message = "Birth day must be no more than 31")
+    private Integer birthDay;
+
+    @Min(value = 1, message = "Birth month must be at least 1")
+    @Max(value = 12, message = "Birth month must be no more than 12")
+    private Integer birthMonth;
+
+    @Positive(message = "Birth year must be a positive number")
+    private Integer birthYear;
+
+    private String phoneNumber;
+
+    private String shippingStreet;
+
+    private String shippingCity;
+
+    private String shippingZipCode;
+
+    private String shippingState;
+
+    private String shippingCountry;
+
+    private String cardName;
+
+    @Pattern(regexp = "^[0-9]*$", message = "Card number must contain only digits.")
+    @Size(min = 13, max = 19, message = "Card number must contain between 13 and 19 digits.")
+    private String cardNumber;
+
+    @Min(value = 1, message = "Expiration month must be at least 1")
+    @Max(value = 12, message = "Expiration month must be no more than 12")
+    private Integer expirationMonth;
+
+    @Positive(message = "Birth year must be at least 1")
+    private Integer expirationYear;
+
+    @Min(value = 100, message = "CVV must be at least 100")
+    @Max(value = 9999, message = "CVV must be no more than 9999")
+    private Integer cvc;
+}
