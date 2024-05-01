5e224700515a4e50c18b0cfc576e58805cf35a6f
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-01 14:19:42 +0200

    Code cleanup

--- backend/src/main/java/ba/atlant/auctionapp/enumeration/Role.java
+++ backend/src/main/java/ba/atlant/auctionapp/enumeration/Role.java
@@ -1,6 +1,5 @@
 package ba.atlant.auctionapp.enumeration;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
