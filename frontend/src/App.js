30c958072a0b89ddc2ce0285c289691d2fea0cd6
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-02 02:00:14 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import Router from './components/utilities/Router';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        const handleUnload = () => {
+            localStorage.clear();
+        };
+
+        window.addEventListener('unload', handleUnload);
+
+        return () => {
+            window.removeEventListener('unload', handleUnload);
+        };
+    }, []);
     return <RouterProvider router={Router} />;
 };
 export default App;
