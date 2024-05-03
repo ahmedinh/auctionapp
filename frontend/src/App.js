43842b37552be71d8048739cad18aaa5497a856e
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-03 19:30:59 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import Router from './components/utilities/Router';
-import { useEffect } from 'react';
 
 function App() {
     return <RouterProvider router={Router} />;
