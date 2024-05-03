e464f7b180edf145a7466619176c8cfbbdc8850a
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-03 10:39:43 +0200

    Bug fix - social icons not leading to pages

--- frontend/src/assets/icons/SocialIcons.jsx
+++ frontend/src/assets/icons/SocialIcons.jsx
@@ -3,11 +3,27 @@ import "./SocialIcons.scss";
 import { Icon } from '@iconify/react';
 
 const SocialIcons = () => {
+    const handleSocialClick = (url) => {
+        window.open(url, "_blank");
+    };
+
     return (
         <div className="icons">
-            <Icon icon="akar-icons:facebook-fill" className='icon' />
-            <Icon icon="entypo-social:instagram-with-circle" className='icon' />
-            <Icon icon="entypo-social:twitter-with-circle" className='icon' />
+            <Icon 
+                icon="akar-icons:facebook-fill" 
+                className='icon' 
+                onClick={() => handleSocialClick('https://www.facebook.com')}
+            />
+            <Icon 
+                icon="entypo-social:instagram-with-circle" 
+                className='icon' 
+                onClick={() => handleSocialClick('https://www.instagram.com')}
+            />
+            <Icon 
+                icon="entypo-social:twitter-with-circle" 
+                className='icon' 
+                onClick={() => handleSocialClick('https://www.twitter.com')}
+            />
         </div>
     );
 };
