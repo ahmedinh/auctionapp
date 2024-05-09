6907429f02b0fdbb0321447fc698d876415a8ad7
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-09 21:11:41 +0200

    Update Accordion.jsx

--- frontend/src/components/pages/my-account/profile/Accordion.jsx
+++ frontend/src/components/pages/my-account/profile/Accordion.jsx
@@ -15,23 +15,7 @@ const AccordionExpandIcon = () => {
     const [selectedFile, setSelectedFile] = useState(null);
     const fileInputRef = useRef(null);
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [birthDay, setBirthDay] = useState('');
-    const [birthMonth, setBirthMonth] = useState('');
-    const [birthYear, setBirthYear] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [cardName, setCardName] = useState('');
-    const [cardNumber, setCardNumber] = useState('');
-    const [expirationMonth, setExpirationMonth] = useState('');
-    const [expirationYear, setExpirationYear] = useState('');
-    const [cvc, setCVC] = useState('');
-    const [street, setStreet] = useState('');
-    const [city, setCity] = useState('');
-    const [zipCode, setZipCode] = useState('');
-    const [state, setState] = useState('');
-    const [country, setCountry] = useState('');
+    const [person, setPerson] = useState();
 
 
     const userInfo = useUserInfoGet();
@@ -54,48 +38,23 @@ const AccordionExpandIcon = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         const payload = {
-            firstName,
-            lastName,
-            email,
-            birthDay: parseInt(birthDay),
-            birthMonth: parseInt(birthMonth, 10),
-            birthYear: parseInt(birthYear, 10),
-            phoneNumber,
-            cardName,
-            cardNumber,
-            expirationMonth: parseInt(expirationMonth, 10),
-            expirationYear: parseInt(expirationYear, 10),
-            cvc: parseInt(cvc, 10),
-            shippingStreet: street,
-            shippingCity: city,
-            shippingZipCode: zipCode,
-            shippingState: state,
-            shippingCountry: country,
+            ...person
         };
         changeUserInfoMutation.mutate(payload);
     }
 
     useEffect(() => {
         if (userInfo.data) {
-            setFirstName(userInfo.data.firstName);
-            setLastName(userInfo.data.lastName);
-            setEmail(userInfo.data.email);
-            setBirthDay(userInfo.data.birthDay);
-            setBirthMonth(userInfo.data.birthMonth);
-            setBirthYear(userInfo.data.birthYear);
-            setPhoneNumber(userInfo.data.phoneNumber);
-            setCardName(userInfo.data.cardName);
-            setCardNumber(userInfo.data.cardNumber);
-            setExpirationMonth(userInfo.data.expirationMonth);
-            setExpirationYear(userInfo.data.expirationYear);
-            setCVC(userInfo.data.cvc);
-            setStreet(userInfo.data.shippingStreet);
-            setCity(userInfo.data.shippingCity);
-            setZipCode(userInfo.data.shippingZipCode);
-            setState(userInfo.data.shippingState);
-            setCountry(userInfo.data.country);
+            setPerson(userInfo.data);
         }
     }, [userInfo.data]);
+
+    const handleOnChangeField = (fieldName, value) => {
+        setPerson(prev => ({
+            ...prev,
+            [fieldName]: value
+        }))
+    }
 
     return (
         <div className='accordion-section'>
@@ -127,28 +86,28 @@ const AccordionExpandIcon = () => {
                         <div className="information-section">
                             <div className="first-name-section">
                                 <p>First Name</p>
-                                <input type="text" placeholder='John' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                                <input type="text" placeholder='John' value={person?.firstName || ''} onChange={(e) => handleOnChangeField('firstName', e.target.value)} />
                             </div>
                             <div className="last-name-section">
                                 <p>Last Name</p>
-                                <input type="text" placeholder='Doe' value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                                <input type="text" placeholder='Doe' value={person?.lastName || ''} onChange={(e) => handleOnChangeField('lastName', e.target.value)} />
                             </div>
                             <div className="email-section">
                                 <p>Email Address</p>
-                                <input type="text" placeholder='user@domain.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="text" placeholder='user@domain.com' value={person?.email || ''} onChange={(e) => handleOnChangeField('email', e.target.value)} />
                             </div>
                             <div className="date-section">
                                 <p>Date of Birth</p>
                                 <div className="date-of-birth">
-                                    <input type="number" placeholder='DD' name="" id="" value={birthDay} onChange={(e) => setBirthDay(e.target.value)} />
-                                    <input type="number" placeholder='MM' name="" id="" value={birthMonth} onChange={(e) => setBirthMonth(e.target.value)} />
-                                    <input type="number" placeholder='YYYY' name="" id="" value={birthYear} onChange={(e) => setBirthYear(e.target.value)} />
+                                    <input type="number" placeholder='DD' name="" id="" value={person?.birthDay || ''} onChange={(e) => handleOnChangeField('birthDay', e.target.value)} />
+                                    <input type="number" placeholder='MM' name="" id="" value={person?.birthMonth || ''} onChange={(e) => handleOnChangeField('birthMonth', e.target.value)} />
+                                    <input type="number" placeholder='YYYY' name="" id="" value={person?.birthYear || ''} onChange={(e) => handleOnChangeField('birthYear', e.target.value)} />
                                 </div>
                             </div>
                             <div className="phone-section">
                                 <p>Phone Number</p>
                                 <div className="for-number">
-                                    <input type="text" placeholder='+32534231564' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                                    <input type="text" placeholder='+32534231564' value={person?.phoneNumber || ''} onChange={(e) => handleOnChangeField('phoneNumber', e.target.value)} />
                                     <div className="not-verified">
                                         Not verified
                                     </div>
@@ -174,25 +133,25 @@ const AccordionExpandIcon = () => {
                                 <p>
                                     Name on Card
                                 </p>
-                                <input type="text" placeholder='JOHN DOE' value={cardName} onChange={(e) => setCardName(e.target.value)} />
+                                <input type="text" placeholder='JOHN DOE' value={person?.cardName} onChange={(e) => handleOnChangeField('cardName', e.target.value)} />
                             </div>
                             <div className="card-number">
                                 <p>
                                     Card Number
                                 </p>
-                                <input type="text" placeholder='XXXX-XXXX-XXXX-XXXX' value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
+                                <input type="text" placeholder='XXXX-XXXX-XXXX-XXXX' value={person?.cardNumber} onChange={(e) => handleOnChangeField('cardNumber', e.target.value)} />
                             </div>
                             <div className="expiration-cvc">
                                 <div className="exp-date">
                                     <p>Expiration Date</p>
-                                    <input type="number" placeholder='MM' value={expirationMonth} onChange={(e) => setExpirationMonth(e.target.value)} />
+                                    <input type="number" placeholder='MM' value={person?.expirationMonth || ''} onChange={(e) => handleOnChangeField('expirationMonth', e.target.value)} />
                                 </div>
                                 <div className="year-input">
-                                    <input type="number" placeholder='YY' value={expirationYear} onChange={(e) => setExpirationYear(e.target.value)} />
+                                    <input type="number" placeholder='YY' value={person?.expirationYear || ''} onChange={(e) => handleOnChangeField('expirationYear', e.target.value)} />
                                 </div>
                                 <div className="CVC-input">
                                     <p>CVC/CVV</p>
-                                    <input type="number" placeholder='***' value={cvc} onChange={(e) => setCVC(e.target.value)} />
+                                    <input type="number" placeholder='***' value={person?.cvc || ''} onChange={(e) => handleOnChangeField('cvc', e.target.value)} />
                                 </div>
                             </div>
                         </div>
@@ -213,25 +172,25 @@ const AccordionExpandIcon = () => {
                         <div className="all-fields">
                             <div className="street">
                                 <p>Street</p>
-                                <input type="text" placeholder='123 Main Street' value={street} onChange={(e) => setStreet(e.target.value)} />
+                                <input type="text" placeholder='123 Main Street' value={person?.shippingStreet} onChange={(e) => handleOnChangeField('shippingStreet', e.target.value)} />
                             </div>
                             <div className="city">
                                 <div className="city-part">
                                     <p>City</p>
-                                    <input type="text" placeholder='eg. Madrid' value={city} onChange={(e) => setCity(e.target.value)} />
+                                    <input type="text" placeholder='eg. Madrid' value={person?.shippingCity || ''} onChange={(e) => handleOnChangeField('shippingCity', e.target.value)} />
                                 </div>
                                 <div className="zip-code-part">
                                     <p>Zip Code</p>
-                                    <input type="text" placeholder='XXXXXXX' value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+                                    <input type="text" placeholder='XXXXXXX' value={person?.shippingZipCode || ''} onChange={(e) => handleOnChangeField('shippingZipCode', e.target.value)} />
                                 </div>
                             </div>
                             <div className="state">
                                 <p>State</p>
-                                <input type="text" placeholder='eg. Asturias' value={state} onChange={(e) => setState(e.target.value)} />
+                                <input type="text" placeholder='eg. Asturias' value={person?.shippingState || ''} onChange={(e) => handleOnChangeField('shippingState', e.target.value)} />
                             </div>
                             <div className="country">
                                 <p>Country</p>
-                                <input type="text" placeholder='eg. Spain' value={country} onChange={(e) => setCountry(e.target.value)} />
+                                <input type="text" placeholder='eg. Spain' value={person?.shippingCountry || ''} onChange={(e) => handleOnChangeField('shippingCountry', e.target.value)} />
                             </div>
                         </div>
                     </div>
