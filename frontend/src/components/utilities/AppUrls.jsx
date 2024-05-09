7d0cece6826f1e96a92bf85a0c111ece483e7d52
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-09 20:29:36 +0200

    hooks extracted to specific files

--- frontend/src/components/utilities/AppUrls.jsx
+++ frontend/src/components/utilities/AppUrls.jsx
@@ -11,3 +11,4 @@ export const sellerSoldRoute = myAccountRoute + "seller/sold"
 export const addProductInfoRoute = myAccountRoute + "add-item/product-info"
 
 export const myProfileRoute = myAccountRoute + "profile"
+export const myAccountRoute = "/my-account/"
