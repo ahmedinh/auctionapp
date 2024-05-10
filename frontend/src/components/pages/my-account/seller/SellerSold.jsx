6faee59d3f55515c9ab40c96e4c4f3d4fbb96083
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-10 09:43:21 +0200

    removed separate components

--- frontend/src/components/pages/my-account/seller/SellerSold.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import SellerTable from "./SellerTable";
-import { getSoldUserProducts } from "../../../../api/productsApi";
-
-export default function SellerSold() {
-    return <SellerTable fetchProducts={getSoldUserProducts} queryString="sold-products-user" />;
-}
