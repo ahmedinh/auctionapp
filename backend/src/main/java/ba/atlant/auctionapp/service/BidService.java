20d12b7173f6d97dc987e4c2352ef63c008086a5
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-14 10:20:55 +0200

    refactoring

--- backend/src/main/java/ba/atlant/auctionapp/service/BidService.java
+++ backend/src/main/java/ba/atlant/auctionapp/service/BidService.java
@@ -35,7 +35,7 @@ public class BidService {
     public ResponseEntity<List<BidProjection>> getUserBids(String token) {
         Long userId = Long.valueOf(jwtUtils.getUserIdFromJwtToken(token.substring(7)));
         personRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("No user found with provided ID."));
-        return ResponseEntity.ok(bidRepository.getUserBids(Long.valueOf(userId)));
+        return ResponseEntity.ok(bidRepository.getUserBids(userId));
     }
 
     private boolean isBidValid(BidRequest bidRequest, Product product) {
