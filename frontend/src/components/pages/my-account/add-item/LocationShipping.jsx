e2191a60260cfb2b17ee8a6f644711c2f3221a30
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-17 15:05:23 +0200

    exported mutations to a file

--- frontend/src/components/pages/my-account/add-item/LocationShipping.jsx
+++ frontend/src/components/pages/my-account/add-item/LocationShipping.jsx
@@ -43,41 +43,6 @@ export default function LocationShipping() {
         navigate('/my-account/add-item/product-price');
     }
 
-    const deleteProductMutation = useMutation({
-        mutationKey: ['delete-product-error'],
-        mutationFn: ({ productName }) => deleteProduct({ productName: productName }),
-        onSuccess: () => {
-            alert('Product deleted successfully')
-        },
-        onError: (error) => {
-            console.error('Error deleting product:', error);
-        }
-    })
-
-    const addPicturesMutation = useMutation({
-        mutationKey: ['adding-product-pictures'],
-        mutationFn: ({ uploadedImages, productName }) => addPicturesToProduct({ productPictures: uploadedImages, productName: productName }),
-        onSuccess: () => {
-            alert('Product added successfully');
-            clearSessionStorageProduct();
-            navigate('/home/new-arrivals');
-        },
-        onError: (error) => {
-            console.error('Error creating product:', error);
-            deleteProductMutation.mutate({ productName });
-        }
-    })
-
-    const createProductMutation = useMutation({
-        mutationKey: ['creation-of-product'],
-        mutationFn: (productData) => createProduct({ productData }),
-        onSuccess: () => {
-            addPicturesMutation.mutate({ uploadedImages, productName: productName });
-        },
-        onError: (error) => {
-            console.error('Error creating product:', error);
-        }
-    });
 
     const handleDone = () => {
         const savedData = sessionStorage.getItem('productInfo');
