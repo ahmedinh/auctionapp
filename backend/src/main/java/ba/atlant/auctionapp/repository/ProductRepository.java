656f49e621b0bea9704b716cd63606dac262202d
AHMEDIN HASANOVIC <ahmedinhasanovic2000@gmail.com>
2024-05-21 21:35:41 +0200

    Update ProductRepository.java

--- backend/src/main/java/ba/atlant/auctionapp/repository/ProductRepository.java
+++ backend/src/main/java/ba/atlant/auctionapp/repository/ProductRepository.java
@@ -167,10 +167,14 @@ public interface ProductRepository extends JpaRepository<Product, Long>, PagingA
             p.color as color,
             i.url as url
             FROM Product p
-            INNER JOIN ProductPicture i
-            ON p.id = i.product.id
+            INNER JOIN ProductPicture i ON p.id = i.product.id
+            INNER JOIN SubCategory s ON p.subCategory.id = s.id
+            INNER JOIN Category c ON s.category.id = c.id
             WHERE i.id = ((SELECT MIN(ii.id) FROM ProductPicture ii WHERE ii.product.id = p.id))
-            AND LOWER(p.name) LIKE CONCAT('%', LOWER(:query), '%')
+            AND (LOWER(p.name) LIKE CONCAT('%', LOWER(:query), '%')
+            OR LOWER(p.description) LIKE CONCAT('%', LOWER(:query), '%')
+            OR LOWER(s.name) LIKE CONCAT('%', LOWER(:query), '%')
+            OR LOWER(c.name) LIKE CONCAT('%', LOWER(:query), '%'))
             """)
     Page<ProductProjection> searchProducts(@Param("query") String query, Pageable pageable);
 
@@ -186,10 +190,14 @@ public interface ProductRepository extends JpaRepository<Product, Long>, PagingA
             p.color as color,
             i.url as url
             FROM Product p
-            INNER JOIN ProductPicture i
-            ON p.id = i.product.id
+            INNER JOIN ProductPicture i ON p.id = i.product.id
+            INNER JOIN SubCategory s ON p.subCategory.id = s.id
+            INNER JOIN Category c ON s.category.id = c.id
             WHERE i.id = ((SELECT MIN(ii.id) FROM ProductPicture ii WHERE ii.product.id = p.id))
-            AND LOWER(p.name) LIKE CONCAT('%', LOWER(:query), '%')
+            AND (LOWER(p.name) LIKE CONCAT('%', LOWER(:query), '%')
+            OR LOWER(p.description) LIKE CONCAT('%', LOWER(:query), '%')
+            OR LOWER(s.name) LIKE CONCAT('%', LOWER(:query), '%')
+            OR LOWER(c.name) LIKE CONCAT('%', LOWER(:query), '%'))
             ORDER BY (CASE WHEN p.auctionEnd >= CURRENT_TIMESTAMP THEN 0 ELSE 1 END), p.auctionEnd ASC
             """)
     Page<ProductProjection> searchProductsWithFutureAuctionEnd(@Param("query") String query, Pageable pageable);
