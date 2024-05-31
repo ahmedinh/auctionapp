7fe144e8dbb3e877e95729856e57c54a6fbd2d00
ahmedinh <ahmedinhasanovic2000@gmail.com>
2024-05-31 11:06:49 +0200

    Switched from navlink to navigate for consistency

--- frontend/src/components/pages/HomePage/Products/ProductCard.jsx
+++ frontend/src/components/pages/HomePage/Products/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import "./ProductCard.scss";
 import DollarSign from '../../../../assets/dollar-sign-2.png';
 
@@ -7,27 +7,31 @@ const ProductCard = ({ product, width, height, grid }) => {
     const navigate = useNavigate();
     const productLink = `/shop/product/${product.id}`;
     return (
-        (grid ? (<div className="product" key={product.id} style={{ width: "267.45px" }}>
-            <NavLink to={productLink} style={{ textDecoration: 'none', color: 'inherit' }}>
-                <img src={product.url} alt={product.name} className='picture' style={{ width: width, height: height }} />
-                <h5>{product.name}</h5>
-            </NavLink>
-            <p>Start from <span className="price">${product.startPrice.toFixed(2)}</span></p>
-        </div>) : (<div className="product-in-list">
-            <img className='product-list-image' src={product.url} alt={product.name} onClick={() => navigate(productLink)} />
-            <div className="product-list-content">
-                <div className="headline-and-description" onClick={() => navigate(productLink)}>
+        (grid ? (
+            <div className="product" key={product.id}>
+                <div className='picture-and-name' onClick={() => navigate(productLink)} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <img src={product.url} alt={product.name} className='picture' style={{ width: width, height: height }} />
                     <h5>{product.name}</h5>
-                    <p className="product-list-description">{product.description}</p>
                 </div>
-                {product.highestBid ?
-                    (<p className="start-price">Highest bid ${product.highestBid.toFixed(2)}</p>) :
-                    (<p className="start-price">Start from ${product.startPrice.toFixed(2)}</p>)}
-                <button className="bid-button" onClick={() => navigate(productLink)}>
-                    Bid <img src={DollarSign} alt="" />
-                </button>
+                <p>Start from <span className="price">${product.startPrice.toFixed(2)}</span></p>
             </div>
-        </div>))
+        ) : (
+            <div className="product-in-list">
+                <img className='product-list-image' src={product.url} alt={product.name} onClick={() => navigate(productLink)} />
+                <div className="product-list-content">
+                    <div className="headline-and-description" onClick={() => navigate(productLink)}>
+                        <h5>{product.name}</h5>
+                        <p className="product-list-description">{product.description}</p>
+                    </div>
+                    {product.highestBid ?
+                        (<p className="start-price">Highest bid ${product.highestBid.toFixed(2)}</p>) :
+                        (<p className="start-price">Start from ${product.startPrice.toFixed(2)}</p>)}
+                    <button className="bid-button" onClick={() => navigate(productLink)}>
+                        Bid <img src={DollarSign} alt="" />
+                    </button>
+                </div>
+            </div>
+        ))
     );
 };
 export default ProductCard;
