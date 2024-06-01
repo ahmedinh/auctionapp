ef61b968c5adc3cba29e8e7c6268de8b91ebe265
ahmedinh <ahmedinhasanovic2000@gmail.com>
2024-06-01 12:42:46 +0200

    Updated script for dropping database

--- dropping.sql
+++ dropping.sql
@@ -1,8 +1,11 @@
 drop table flyway_schema_history;
 drop table wish_list;
 drop table bid;
+drop table credit_card;
 drop table product_picture;
 drop table product;
 drop table person;
 drop table sub_category;
 drop table category;
+drop function calculate_levenshtein;
+drop function get_time_left;
